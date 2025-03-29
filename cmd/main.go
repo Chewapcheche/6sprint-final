9cf36163a9679ae1dc2b543495a85f98576557a2
Alexandr Shtaev <pugramentum12339@mail.ru>
2025-03-29 18:06:20 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "server:", log.LstdFlags)
+	srv := server.NewServer(logger)
+	logger.Println("Server start via port 8080")
+	if err := srv.HTTP.ListenAndServe(); err != nil {
+		logger.Fatal("server start error:", err)
+	}
 }
